d8399e2213c8dc96d75806b6d9f0c4e4ea9602e3
Marc Mengel <mengel@fnal.gov>
2020-09-10 15:56:11 -0500

    finding packages

--- lib/packages.py
+++ lib/packages.py
@@ -6,9 +6,7 @@ from glob import glob
 def pkg_find(p,qual=''):
     path = None
     if not path and  os.environ.get("SPACK_ROOT",None):
-        print("checking Spack:")
         cmd = "spack find --paths --variants '%s os=fe' 'py-%s os=fe'" % p
-        print("running: %s" % cmd)
         f = os.popen(cmd, "r")
         for line in f:
             if line[0] == '-':
@@ -16,27 +14,19 @@ def pkg_find(p,qual=''):
             path = line.split()[1]
             break
         f.close()
-         
+
     if not path and os.environ.get("PRODUCTS",None):
-        print("checking UPS:")
         cmd = "ups list -a4 -Kproduct:@prod_dir %s %s, -a0 -Kproduct:@prod_dir %s %s" % (p,qual, p,qual)
-        print("running: %s" % cmd)
         f = os.popen(cmd, "r")
         for line in f:
-            print("line: %s" % line)
             path = line.split()[1].strip('"')
             break
         f.close()
-   
+
     if path:
-        print("found packge %s at %s" % (p, path))
-        gl = glob('%s/lib/python*/site-packages' % path)
-        if gl:
-            print("globbed %s" % gl)
-            sys.path.append(gl[0])
-            return
-        gl = glob('%s/lib/python*' % path)
-        if gl:
-            print("globbed %s" % gl)
-            sys.path.append(gl[0])
-            return
+        for fmt in ['%s/lib/python*/site-packages/*.egg', '%s/lib/python*/site-packages', '%s/lib/python*']:
+
+                gl = glob(fmt % path) 
+                if gl:
+                    sys.path = sys.path + gl
+                    return
