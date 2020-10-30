3c12f97b1376c6c5da96ff8e294a6e9eaf928c17
Marc Mengel <mengel@fnal.gov>
2020-10-30 18:17:36 -0500

    add SpoolOnEvict

--- templates/simple/simple.cmd
+++ templates/simple/simple.cmd
@@ -53,4 +53,9 @@ x509userproxy = {{proxy}}
 {{lines|join("\n+")}}
 requirements  = {%if overwrite_requirements %}{{overwrite_requirements}}{%else%}target.machine =!= MachineAttrMachine1 && target.machine =!= MachineAttrMachine2  && (isUndefined(DesiredOS) || stringListsIntersect(toUpper(DesiredOS),IFOS_installed)) && (stringListsIntersect(toUpper(target.HAS_usage_model), toUpper(my.DESIRED_usage_model))){%endif%}{%if append_condor_requirements %} && {{append_condor_requirements}} {%endif%}
 
+#
+# this is supposed to get us output even if jobs are held(?)
+#
++SpoolOnEvict = false
+
 queue {{N}}
