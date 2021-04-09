1d446f6d8e3f180cc91962580f56dbe1fe43be7d
Marc Mengel <mengel@fnal.gov>
2021-04-09 08:29:09 -0500

    cleanup

--- templates/simple/simple.sh
+++ templates/simple/simple.sh
@@ -312,7 +312,7 @@ ${JSB_TMP}/ifdh.sh log poms_data=$poms_data
 echo `date` $JOBSUBJOBID BEGIN EXECUTION $JOBSUB_EXE_SCRIPT {{exe_arguments|join(" ")}} >&2 
 echo `date` $JOBSUBJOBID BEGIN EXECUTION $JOBSUB_EXE_SCRIPT {{exe_arguments|join(" ")}}
 
-{%if timeout%} timeout {{timeout}} {%endif%}  $JOBSUB_EXE_SCRIPT {{exe_arguments|join(" ")}} {%endif%}
+{%if timeout%} timeout {{timeout}} {%endif%} $JOBSUB_EXE_SCRIPT {{exe_arguments|join(" ")}} 
 JOB_RET_STATUS=$?
 
 # copy out -d directories
