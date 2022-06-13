742213310deaa1082f948918e18bf8ac6362accd
Marc Mengel <mengel@fnal.gov>
2022-06-13 11:58:26 -0500

    keep common bits in test_unit.py

---
+++ tests/test_unit.py
@@ -0,0 +1,25 @@
+
+
+class TestUnit:
+    """
+        strings/parameters common to unit tests
+    """
+
+    #test_group = "fermilab"
+    test_group = "dune"
+    test_schedd = "jobsubdevgpvm01.fnal.gov"
+    test_vargs = { 
+            "debug":             False,
+            "executable":         "file:///bin/true",
+            "exe_arguments":     [],
+            "memory":            "64gb",
+            "disk":              "100mb",
+            "expected_lifetime": "8h",
+            "timeout":           "8h",
+            "devserver":         True,
+            "environment":       ["USER"],
+            "group":             test_group,
+            "N":                 1,
+            "resource_provides": "usage_model=OPPORTUNISTIC,DEDICATED,OFFSITE" 
+        }
+
