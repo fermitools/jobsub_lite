a2898892f480bf7e592ccec4647207fa6909e22c
Marc Mengel <mengel@fnal.gov>
2022-07-13 08:45:39 -0500

    check for defined but None or blank

--- templates/dag/dagman_wrapper.sh
+++ templates/dag/dagman_wrapper.sh
@@ -3,7 +3,7 @@
 # condor wants to copy in the condor_dagman executable, but we
 # want it to run the local one, so we give it this one...
 
-{% if role is defined and role != 'Analysis' %}
+{% if role is defined and role and role != 'Analysis' %}
 export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{role}}.use
 {% else %}
 export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}.use
