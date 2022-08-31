a88ecae26770e5bfc0a376ee54e275464bcea8a8
Shreyas Bhat <sbhat@fnal.gov>
2022-08-31 17:00:45 +0000

    Added fixture setup_ups to set up UPS in test environment

--- tests/test_packages_unit.py
+++ tests/test_packages_unit.py
@@ -1,4 +1,6 @@
 import os
+import shlex
+import subprocess
 import sys
 import pytest
 
@@ -16,6 +18,26 @@ sys.path.append("../lib")
 import packages
 
 
+@pytest.fixture
+def setup_ups(monkeypatch):
+    """A fixture to set up UPS for our tests, in case it is not already set up in the test environment.
+    Adapted from https://stackoverflow.com/a/3505826
+    """
+    keys = []
+    command = shlex.split(
+        "env -i bash -c 'source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh && env'"
+    )
+    source_proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line in source_proc.stdout:
+        (env_key, _, env_value) = line.decode().partition("=")
+        keys.append(env_key)
+        monkeypatch.setenv(env_key, env_value)
+    source_proc.communicate()
+    yield
+    for key in keys:
+        monkeypatch.delenv(key)
+
+
 class TestPackagesUnit:
     """
     Use with pytest... unit tests for ../lib/*.py
@@ -24,7 +46,7 @@ class TestPackagesUnit:
     # lib/packages.py routines
 
     @pytest.mark.unit
-    def test_pkg_find_1(self):
+    def test_pkg_find_1(self, setup_ups):
         """make sure we can find the poms_client ups package"""
         sp1 = sys.path.copy()
         packages.pkg_find("poms_client", "-g poms41")
@@ -35,7 +57,7 @@ class TestPackagesUnit:
         __import__("poms_client")
 
     @pytest.mark.unit
-    def test_pkg_orig_env_1(self):
+    def test_pkg_orig_env_1(self, setup_ups):
         """make sure orig_env puts the environment back"""
         packages.pkg_find("poms_client", "-g poms41")
         env1 = os.environ.copy()
