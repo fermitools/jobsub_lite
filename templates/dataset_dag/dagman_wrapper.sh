a4d18968095da4eeeb997c37298476fc13a47552
Marc Mengel <mengel@fnal.gov>
2022-09-07 23:09:21 -0500

    apparently it doesn't do the condor_exec.exe thing for dagman jobs?

--- templates/dataset_dag/dagman_wrapper.sh
+++ templates/dataset_dag/dagman_wrapper.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# so we can be copied back..
-ln $0 dagman_wrapper.sh
-
 # condor wants to copy in the condor_dagman executable, but we
 # want it to run the local one, so we give it this one...
 
