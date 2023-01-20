b78b0b611f408561b19cb8017a2e6e98ca8c2511
Shreyas Bhat <sbhat@fnal.gov>
2023-01-20 17:11:38 +0000

    Lowercase role for BEARER_TOKEN_FILE setting in wrapper scripts

--- templates/simple/simple.sh
+++ templates/simple/simple.sh
@@ -22,8 +22,8 @@ done
 {% endif %}
 
 {% if role is defined and role and role != 'Analysis' %}
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role}}_{{oauth_handle}}.use
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
 {% else %}
 #export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
 export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
