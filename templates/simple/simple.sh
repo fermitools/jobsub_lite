726210302fec2630375dcb3d7d5728b079cea274
Marc Mengel <mengel@fnal.gov>
2023-02-16 16:06:21 -0600

    token weakening again... with patched condor_vault_storer

--- templates/simple/simple.sh
+++ templates/simple/simple.sh
@@ -22,11 +22,11 @@ done
 {% endif %}
 
 {% if role is defined and role and role != 'Analysis' %}
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
 {% else %}
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
 {% endif %}
 
 set_jobsub_debug(){
