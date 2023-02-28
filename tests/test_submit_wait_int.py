4252d176dcd001b23f697c37734e4831677eefc6
Marc Mengel <mengel@fnal.gov>
2023-02-28 11:43:00 -0600

    turn off --onsite in tests so we don't hang on dune global pool

--- tests/test_submit_wait_int.py
+++ tests/test_submit_wait_int.py
@@ -182,7 +182,7 @@ def run_launch(cmd):
 def lookaround_launch(extra, verify_files=""):
     """Simple submit of our lookaround script"""
     assert run_launch(
-        f"jobsub_submit --verbose=1 -e SAM_EXPERIMENT {extra} --onsite file://`pwd`/job_scripts/lookaround.sh {verify_files}"
+        f"jobsub_submit --verbose=1 -e SAM_EXPERIMENT {extra} file://`pwd`/job_scripts/lookaround.sh {verify_files}"
     )
 
 
@@ -259,7 +259,6 @@ def dagnabbit_launch(extra, which=""):
         jobsub_submit \
           --verbose=2 \
           -e SAM_EXPERIMENT {extra} \
-          --onsite \
           --dag file://dagTest{which} \
         """
     )
@@ -305,7 +304,6 @@ def fife_launch(extra):
           -e IFDH_CP_MAXRETRIES \
           -e VERSION \
           -N 5  \
-          --onsite \
           --generate-email-summary \
           --expected-lifetime=2h  \
           --timeout=2h \
