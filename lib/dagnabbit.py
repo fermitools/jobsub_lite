83a72798ccdd25b6e2e7f565f2ea9269a84fb61c
Marc Mengel <mengel@fnal.gov>
2023-03-02 22:40:27 -0600

    fix command-line/dag file merging of list items

--- lib/dagnabbit.py
+++ lib/dagnabbit.py
@@ -141,8 +141,8 @@ def parse_dagnabbit(
                 # (which we will pass in as a dag # job parameter)
                 # to assist compaction...
                 # ONLY do the very end
-                line = re.sub(f"\\b{count-2}\s*$", "$(CM2)", line)
-                line = re.sub(f"\\b{count-1}\s*$", "$(CM1)", line)
+                line = re.sub(f"\\b{count-2}\\s*$", "$(CM2)", line)
+                line = re.sub(f"\\b{count-1}\\s*$", "$(CM1)", line)
 
                 if line == prev_jobsub_line:
 
@@ -197,7 +197,13 @@ def parse_dagnabbit(
                         if not update_with[k]:
                             del update_with[k]
 
-                    thesevalues.update(update_with)
+                    # do not just update, rather update but also merge items that are lists
+                    for k in update_with:
+                        if isinstance(thesevalues.get(k, False), List):
+                            thesevalues[k].extend(update_with[k])
+                        else:
+                            thesevalues[k] = update_with[k]
+
                     set_extras_n_fix_units(thesevalues, schedd_name, proxy, token)
                     thesevalues["script_name"] = f"{name}.sh"
                     thesevalues["cmd_name"] = f"{name}.cmd"
