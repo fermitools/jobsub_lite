337a3a102de3599928f90547267ee68be74f2fa0
Shreyas Bhat <sbhat@fnal.gov>
2023-04-05 16:41:41 +0000

    Condense fake_ifdh tests by using clear_x509_user_proxy fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 import pytest
@@ -20,7 +21,7 @@ import creds
 
 
 @pytest.fixture
-def needs_credentials():
+def needs_credentials(check_user_kerberos_creds):
     os.environ["GROUP"] = TestUnit.test_group
     return creds.get_creds({"role": "Analysis"})
 
@@ -33,3 +34,15 @@ def clear_x509_user_proxy():
 
     if old_x509_user_proxy_value is not None:
         os.environ["X509_USER_PROXY"] = old_x509_user_proxy_value
+
+
+@pytest.fixture
+def check_user_kerberos_creds():
+    """Make sure we have kerberos credentials before starting the test"""
+    proc = subprocess.run(
+        ["klist"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="UTF-8"
+    )
+    if proc.returncode or ("No credentials cache found" in proc.stdout):
+        raise Exception(
+            f"No kerberos credentials found.  Please run kinit and try again.  Error: {proc.stdout}"
+        )
