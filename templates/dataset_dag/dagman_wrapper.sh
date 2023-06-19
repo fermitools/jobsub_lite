31b06d4cd67a2f275e64b0089a6bebc08571598e
Marc Mengel <mengel@fnal.gov>
2023-06-19 16:54:35 -0500

    attempt at local universe job, seems to not be getting a token pushed though

--- templates/dataset_dag/dagman_wrapper.sh
+++ templates/dataset_dag/dagman_wrapper.sh
@@ -6,4 +6,14 @@
 # touch our transfer files so condor will copy them back
 (sleep 1; touch {%for f in transfer_files%}{{f}} {%endfor%}) &
 
+ls -l .condor_creds
+
+{% if role is defined and role and role != 'Analysis' %}
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
+{% else %}
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
+{% endif %}
+
 exec /usr/bin/condor_dagman "$@"
--- templates/maxconcurrent_dag/dagman_wrapper.sh
+++ templates/maxconcurrent_dag/dagman_wrapper.sh
@@ -6,4 +6,13 @@
 # touch our transfer files so condor will copy them back
 (sleep 1; touch {%for f in transfer_files%}{{f}} {%endfor%}) &
 
+
+{% if role is defined and role and role != 'Analysis' %}
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
+{% else %}
+export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
+{% endif %}
+
 exec /usr/bin/condor_dagman "$@"
