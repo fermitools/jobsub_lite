0206c6af6068e5e0dc84dc2f2d1e6dc6c7cd0d69
Marc Mengel <mengel@fnal.gov>
2023-06-21 11:53:18 -0500

    should be working, but isn't still

--- templates/dataset_dag/dagman_wrapper.sh
+++ templates/dataset_dag/dagman_wrapper.sh
@@ -6,14 +6,22 @@
 # touch our transfer files so condor will copy them back
 (sleep 1; touch {%for f in transfer_files%}{{f}} {%endfor%}) &
 
-ls -laR .
+# mengel -- debug bits to get dagman submitting with token authentication
+echo "\$_CONDOR_CREDS=" $_CONDOR_CREDS
+echo "===="
+ls -laR `pwd`
+echo "===="
+ls -laR $_CONDOR_CREDS
+export _condor_ALL_DEBUG=D_ALL
+echo "==="
+printenv | grep '^_condor'
 
 {% if role is defined and role and role != 'Analysis' %}
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
+export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{role | lower}}.use
 {% else %}
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
+export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}.use
 {% endif %}
 
 exec /usr/bin/condor_dagman "$@"
