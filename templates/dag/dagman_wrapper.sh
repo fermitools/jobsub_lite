28ab3cece55a8016bcce56302bc7962bdc0e4c83
Marc Mengel <mengel@fnal.gov>
2023-06-21 11:53:18 -0500

    should be working, but isn't still

--- templates/dag/dagman_wrapper.sh
+++ templates/dag/dagman_wrapper.sh
@@ -9,11 +9,11 @@
 
 
 {% if role is defined and role and role != 'Analysis' %}
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}_{{oauth_handle}}.use
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{role | lower}}.use
+export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{role | lower}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{role | lower}}.use
 {% else %}
-export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}_{{oauth_handle}}.use
-#export BEARER_TOKEN_FILE=$PWD/.condor_creds/{{group}}.use
+export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}_{{oauth_handle}}.use
+#export BEARER_TOKEN_FILE=$_CONDOR_CREDS/{{group}}.use
 {% endif %}
 
 exec /usr/bin/condor_dagman "$@"
