9bb353604b3825e40ef66d85d3ac14a9c2504cd5
Marc Mengel <mengel@fnal.gov>
2024-01-22 16:57:24 -0600

    get rid of import cycle

--- lib/submit_support.py
+++ lib/submit_support.py
@@ -6,9 +6,8 @@ from typing import List, Dict, Any
 
 from condor import submit, submit_dag
 from dagnabbit import parse_dagnabbit
-from fake_ifdh import mkdir_p, cp
 from render_files import render_files
-from tracing import as_span
+from transfer_sandbox import transfer_sandbox
 
 PREFIX = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -61,30 +60,6 @@ def do_dataset_defaults(varg: Dict[str, Any]) -> None:
         varg["environment"].append(f"SAM_USER={os.environ['USER']}")
     if not have_group:
         varg["environment"].append(f"SAM_GROUP={experiment}")
-
-
-@as_span("transfer_sandbox")
-def transfer_sandbox(src_dir: str, dest_url: str) -> None:
-    """Transfer files from src_dir to sandbox with fake_ifdh (gfal-copy).
-    Nothing failing here is considered fatal, since it doesn't affect the job
-    itself, just log availability.
-
-    """
-    print("Transferring files to web sandbox...")
-    try:
-        mkdir_p(dest_url)
-    except Exception as e:  # pylint: disable=broad-except
-        print(
-            f"warning: error creating sandbox, web logs will not be available for this submission: {e}"
-        )
-        return
-    for f in os.listdir(src_dir):
-        try:
-            cp(os.path.join(src_dir, f), os.path.join(dest_url, f))
-        except Exception as e:  # pylint: disable=broad-except
-            print(
-                f"warning: error copying {f} to sandbox, will not be available through web logs: {e}"
-            )
 
 
 def get_env_list(name: str) -> List[str]:
