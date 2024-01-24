eca7a39855550651e834e43f5c6913db512fca1b
Shreyas Bhat <sbhat@fnal.gov>
2024-01-24 09:45:24 -0600

    Fixed bug in __restore_fixed_server_behavior_func where we were not properly storing the old dropbox server.  Also cleaned up some comments

--- lib/tarfiles.py
+++ lib/tarfiles.py
@@ -488,11 +488,20 @@ class TarfilePublisherHandler:
                     self._fixed_server and always_switch_servers
                 )
 
-                if should_change_selector_behavior and self.verbose:
-                    print(
-                        "TarfilePublisherHandler is configured to use a fixed RCDS server, however this is being overridden "
-                        "for the duration of the current request."
-                    )
+                # Default value for restore_func.  In reality we won't use this value at all if should_change_selector_behavior is False
+                restore_func: Callable[
+                    ..., Iterator[str]
+                ] = lambda: self._dropbox_server_selector  # type: ignore
+
+                if should_change_selector_behavior:
+                    restore_func = (
+                        self.__restore_fixed_server_behavior_func()
+                    )  # Evaluate this now so that when we restore the dropbox_server_selector, the resultant iterator is giving us the correct server
+                    if self.verbose:
+                        print(
+                            "TarfilePublisherHandler is configured to use a fixed RCDS server, however this is being overridden "
+                            "for the duration of the current request."
+                        )
 
                 _retry_interval_sec = 0  # First retry immediately, and then we can wait the retry interval
                 # pylint: disable-next=protected-access
@@ -503,7 +512,7 @@ class TarfilePublisherHandler:
                         # pylint: disable=protected-access
                         _dropbox_server = next(self._dropbox_server_selector)
 
-                        # If we're supposed to be switching servers, make sure we are
+                        # If we're supposed to be switching servers, make sure we are actually switching servers
                         if (
                             len(self.dropbox_server_string.split()) > 1
                             and not self._fixed_server
@@ -548,7 +557,7 @@ class TarfilePublisherHandler:
                                 print(
                                     "Restoring fixed server behavior of TarfilePublisherHandler"
                                 )
-                            self.__restore_fixed_server_behavior()
+                            self._dropbox_server_selector = restore_func()
                         break
                 return response
 
@@ -636,7 +645,7 @@ class TarfilePublisherHandler:
         self._dropbox_server_selector = self.__setup_dropbox_server_selector()
 
     def deactivate_server_switcher(self) -> None:
-        """Change TarfilePublisherHandler so that it will try a single PubAPI repeatedly"""
+        """Change TarfilePublisherHandler so that it will try a single PubAPI server repeatedly"""
         self._fixed_server = True
         self._dropbox_server_selector = self.__setup_fixed_dropbox_server()
 
@@ -670,12 +679,32 @@ class TarfilePublisherHandler:
                 "No server was specified to publish the tarball.  Please check to ensure that JOBSUB_DROPBOX_SERVER_LIST is set in the environment"
             )
 
-    def __restore_fixed_server_behavior(self) -> Callable[..., Iterator[str]]:
-        """This function is meant to be used when wanting to restore the behavior
-        of using a fixed dropbox server after activating the dropbox server switcher"""
+    def __restore_fixed_server_behavior_func(self) -> Callable[..., Iterator[str]]:
+        """This function is meant to be used when restoring the behavior
+        of yielding a fixed dropbox server after activating the dropbox server switcher.
+
+        It returns a function that will set self._fixed_server to True and will
+        return an infinite iterator that repeatedly generates the value given by
+        self._dropbox_server_selector at the time this function is called,
+        mimicking the behavior of __setup_fixed_dropbox_server, but ensuring
+        that the yielded values are the same value as before the dropbox
+        server switcher was activated.
+
+        The following is how it should generally be used within the class:
+
+            def func(self):
+                restore_func = self.__restore_fixed_server_behavior_func() # Evaluate this now so that the correct server is saved
+                self.activate_server_switcher()
+                do_stuff()
+                restore_func()
+        """
         __fixed_server = next(self._dropbox_server_selector)
-        self._fixed_server = True
-        return lambda: itertools.repeat(__fixed_server)
+
+        def return_func() -> Iterator[str]:
+            self._fixed_server = True
+            return itertools.repeat(__fixed_server)
+
+        return return_func
 
 
 class NoPublisherHandlerServerError(Exception):
