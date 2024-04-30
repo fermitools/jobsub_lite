115727dd2d354331dbbd87c9b93c82eb69bb9e37
Shreyas Bhat <sbhat@fnal.gov>
2024-04-30 14:08:47 -0500

    Bump version to 1.8

--- lib/version.py
+++ lib/version.py
@@ -19,7 +19,7 @@ __title__ = "jobsub_lite"
 __summary__ = "The local HTCondor job submission software for Fermilab users to submit jobs to local FermiGrid resources and to the Open Science Grid."
 __uri__ = "https://fifewiki.fnal.gov/wiki/Jobsub_Lite"
 
-__version__ = "1.7.2"
+__version__ = "1.8"
 __email__ = "jobsub-support@fnal.gov"
 
 __license__ = "Apache License, Version 2.0"
