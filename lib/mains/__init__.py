b77f8be8b6027504d722c193a7a7904b8c2f549e
Marc Mengel <mengel@fnal.gov>
2025-01-31 11:01:23 -0600

    splitting up mains.py

---
+++ lib/mains/__init__.py
@@ -0,0 +1,4 @@
+from .common import VERBOSE
+from .cmd import jobsub_cmd_parser, jobsub_cmd_main, jobsub_cmd_args
+from .fetchlog import jobsub_fetchlog_parser, jobsub_fetchlog_main, jobsub_fetchlog_args
+from .submit import jobsub_submit_main, jobsub_submit_args
