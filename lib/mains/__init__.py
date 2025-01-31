fa0d44d7f3217c5ce4d0df04aaca1f2022c498fc
Marc Mengel <mengel@fnal.gov>
2025-01-31 12:49:54 -0600

    add __all__ to mains/__init__.py

--- lib/mains/__init__.py
+++ lib/mains/__init__.py
@@ -2,3 +2,15 @@ from .common import VERBOSE
 from .cmd import jobsub_cmd_parser, jobsub_cmd_main, jobsub_cmd_args
 from .fetchlog import jobsub_fetchlog_parser, jobsub_fetchlog_main, jobsub_fetchlog_args
 from .submit import jobsub_submit_main, jobsub_submit_args
+
+__all__ = [
+    "VERBOSE",
+    "jobsub_cmd_parser",
+    "jobsub_cmd_main",
+    "jobsub_cmd_args",
+    "jobsub_fetchlog_parser",
+    "jobsub_fetchlog_main",
+    "jobsub_fetchlog_args",
+    "jobsub_submit_main",
+    "jobsub_submit_args",
+]
